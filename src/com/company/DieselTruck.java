58970bfede63320109f3ac8139e9edd1f948157c
solskinIsak <cph-mm712@cphbusiness.dk>
2022-03-28 11:30:17 +0200

    Added the class diesel truck and put a constructor matching the super inside.

---
+++ src/com/company/DieselTruck.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class DieselTruck extends DieselCar
+{
+
+    public DieselTruck(int kmPrLitre, String make, String model, String registrationNumber, int numberOfDoors, boolean hasParticleFilter) {
+        super(kmPrLitre, make, model, registrationNumber, numberOfDoors, hasParticleFilter);
+    }
+}
